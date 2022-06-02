55c1613ec5ba01f396be01c33979893e3ae99551
Tony Stark Jr <st.juniordeveloper@gmail.com>
2022-06-02 12:32:41 +0545

    after completing first challange

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { React } from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <>
+        <h1>My Netflix Pick</h1>
+        <p>List of 5 Best Movies</p>
+        <ol>
+            <li>Extraction</li>
+            <li>13 hours</li>
+            <li>Ambulance</li>
+            <li>Red Notice</li>
+            <li>6 Underground</li>
+        </ol>
+    </>,
+    document.getElementById("root")
+)

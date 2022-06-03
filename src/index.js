76d65a1cff480ec93a6a803c0fe6c29fa8768987
Tony Stark Jr <st.juniordeveloper@gmail.com>
2022-06-03 14:24:18 +0545

    React JS Mini-Project #1: Creating Simple Greeting Website using React JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"
+import "./index.css"
 
-let date = new Date();
+let curDate = new Date();
+curDate = curDate.getHours();
+let greeting = "";
+let cssStyle = {};
+if (curDate >= 1 && curDate < 12) {
+    greeting = "Good Morning"
+    cssStyle.color = "green"
+} else if (curDate >= 12 && curDate < 20) {
+    greeting = "Good Afternoon"
+    cssStyle.color = "orange"
+
+} else {
+    greeting = "Good Night"
+}
+
 ReactDOM.render(
-    <div>
-        <h1>My Name is Santosh Marar</h1>
-        <p>Today Date is {date.toDateString()}</p>
-        <p>Current Time is {date.toLocaleTimeString()}</p>
-    </div>,
+    <>
+        <p>Hello Sir,<span style={cssStyle}>{greeting}</span></p>
+    </>,
     document.getElementById("root")
 )
